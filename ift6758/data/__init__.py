2a6ac0717158cee6b14c81ee101fbb005785cf04
Jay-D13 <jaydaninparis@gmail.com>
2023-10-05 17:50:01 -0400

    fixed code

--- ift6758/data/__init__.py
+++ ift6758/data/__init__.py
@@ -1,18 +1,4 @@
-"""
-You can "pre" import stuff in the __init__.py module, but this is not required.
-This allows you to import this function as:
-
-    from ift6758.data import get_player_stats
-
-instead of only:
-
-    from ift6758.data.question_1 import get_player_stats
-
-but both are still valid. You can read more about packages and modules here:
-https://docs.python.org/3/reference/import.html#regular-packages
-"""
-from .question_1 import get_player_stats
 
 NHL_GAME_URL = 'https://statsapi.web.nhl.com/api/v1/game/{GAME_ID}/feed/live/'
 NB_MAX_REGULAR_GAMES_PER_SEASON = 1353
-NB_MAX_PLAYOFF_GAMES_PER_SEASON = 130
+NB_MAX_PLAYOFF_GAMES_PER_SEASON = 1353
--- ift6758/data/acquisition.py
+++ ift6758/data/acquisition.py
@@ -1,18 +1,23 @@
-import os, requests, tqdm, pickle
+import os
+import requests
+import pickle
+import json
 import pandas as pd
-from ift6758.data import NB_MAX_REGULAR_GAMES_PER_SEASON, NB_MAX_PLAYOFF_GAMES_PER_SEASON
 from enum import Enum
+from tqdm import tqdm
+from ift6758.data import NB_MAX_REGULAR_GAMES_PER_SEASON, NB_MAX_PLAYOFF_GAMES_PER_SEASON, NHL_GAME_URL
 
 class SeasonType(Enum):
     REGULAR = '02'
     PLAYOFF = '03'
+    
 class NHLGameData:
-    def __init__(self, data_path, base_url="https://statsapi.web.nhl.com/api/v1/game/{GAME_ID}/feed/live/"):
-        if not os.path.exists(data_path):
-            os.makedirs(data_path)
+    def __init__(self, data_path, base_url=NHL_GAME_URL):
         self.base_url = base_url
         self.data_path = data_path
         self.cache = {}
+        
+        os.makedirs(data_path, exist_ok=True)
         
     def __add__(self, other):
         new_instance = NHLGameData(data_path=self.data_path)
@@ -21,44 +26,43 @@ class NHLGameData:
     
     def _fetch_game_from_api(self, url):
         response = requests.get(url)
-        
         if response.status_code != 200:
             raise Exception(f"Failed to retrieve {url}. Status code: {response.status_code}")
         
-        with open(self.data_path, 'wb') as out_file:
-            pickle.dump(self.data, out_file)
+        return response.json()
             
-    
     def fetch_game(self, season, game_type, game_num):
-        game_id = f"{season}{game_type}{game_num}"
+        game_id = f"{season}{game_type.value}{game_num:04d}"
         url = self.base_url.format(GAME_ID=game_id)
         
         if game_id in self.cache:
             return self.cache[game_id]
-        
-        if os.path.exists(self.data_path):
-            with open(self.data_path, 'r') as file:
-                data = pd.read_json(file)
-                self.cache[game_id] = data
-                return data
-        
-        print(f"Retrieving data from '{url}'...")
-        data = self._fetch_data_from_api(url)
-        
-        self.cache[game_id] = pd.DataFrame(data)
-        return self.cache[game_id]
+
+        # Check locally
+        local_file_path = os.path.join(self.data_path, f"{game_id}.json")
+        if os.path.exists(local_file_path):
+            with open(local_file_path, 'r') as file:
+                data = json.load(file)
+        else: # Get from API
+            print(f"Retrieving data from: {url}...")
+            data = self._fetch_game_from_api(url)
+            
+            with open(local_file_path, 'w') as out_file:
+                json.dump(data, out_file)
+                
+        df = pd.DataFrame(data)
+        self.cache[game_id] = df
+        return df
     
     def fetch_season(self, season, regular_games=NB_MAX_REGULAR_GAMES_PER_SEASON, playoff_games=NB_MAX_PLAYOFF_GAMES_PER_SEASON):
 
         # Fetch regular season games
-        for i in tqdm(range(1, regular_games + 1)).set_description("Fetching regular season games"):
-            game_num = f"{i:04d}"
+        for game_num in tqdm(range(1, regular_games + 1), desc="Fetching regular games"):
             print(f"Fetching game {game_num} of {regular_games}")
             self.fetch_game(season, SeasonType.REGULAR, game_num)
         
         # Fetch playoff games
-        for game_num in tqdm(range(1, playoff_games + 1)).set_description("Fetching playoff games"):
-            game_num = f"{i:04d}"
+        for game_num in tqdm(range(1, playoff_games + 1), desc="Fetching playoff games"):
             print(f"Fetching game {game_num} of {playoff_games}")
             self.fetch_game(season, SeasonType.PLAYOFF, game_num)
-            
+    
