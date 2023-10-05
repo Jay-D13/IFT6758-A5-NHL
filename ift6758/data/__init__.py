81ee9c4e308539786f20adb503dd35dc8257619f
Jay-D13 <jaydaninparis@gmail.com>
2023-10-05 15:06:15 -0400

    question 1 telechargement data inspired de ce que Guillaume a fait

--- ift6758/data/__init__.py
+++ ift6758/data/__init__.py
@@ -12,3 +12,7 @@ but both are still valid. You can read more about packages and modules here:
 https://docs.python.org/3/reference/import.html#regular-packages
 """
 from .question_1 import get_player_stats
+
+NHL_GAME_URL = 'https://statsapi.web.nhl.com/api/v1/game/{GAME_ID}/feed/live/'
+NB_MAX_REGULAR_GAMES_PER_SEASON = 1353
+NB_MAX_PLAYOFF_GAMES_PER_SEASON = 130
--- ift6758/data/question_1.py
+++ ift6758/data/question_1.py
@@ -1,39 +1,59 @@
+import os, requests, tqdm
 import pandas as pd
+from ift6758.data import NB_MAX_REGULAR_GAMES_PER_SEASON, NB_MAX_PLAYOFF_GAMES_PER_SEASON
 
-def get_player_stats(year: int, player_type: str) -> pd.DataFrame:
-    """
-
-    Uses Pandas' built in HTML parser to scrape the tabular player statistics from
-    https://www.hockey-reference.com/leagues/ . If the player played on multiple 
-    teams in a single season, the individual team's statistics are discarded and
-    the total ('TOT') statistics are retained (the multiple team names are discarded)
-
-    Args:
-        year (int): The first year of the season to retrieve, i.e. for the 2016-17
-            season you'd put in 2016
-        player_type (str): Either 'skaters' for forwards and defensemen, or 'goalies'
-            for goaltenders.
-    """
-
-    if player_type not in ["skaters", "goalies"]:
-        raise RuntimeError("'player_type' must be either 'skaters' or 'goalies'")
+class NHLGameData:
+    def __init__(self, data_path, base_url="https://statsapi.web.nhl.com/api/v1/game/{GAME_ID}/feed/live/"):
+        if not os.path.exists(data_path):
+            os.makedirs(data_path)
+        self.base_url = base_url
+        self.data_path = data_path
+        self.cache = {}
+        
+    def __add__(self, other):
+        new_instance = NHLGameData(data_path=self.data_path)
+        new_instance.cache = {**self.cache, **other.cache}
+        return new_instance
     
-    url = f'https://www.hockey-reference.com/leagues/NHL_{year}_{player_type}.html'
+    def _fetch_game_from_api(self, url):
+        response = requests.get(url)
+        
+        if response.status_code != 200:
+            raise Exception(f"Failed to retrieve {url}. Status code: {response.status_code}")
+        
+        with open(self.data_path, 'w') as file:
+            file.write(response.text)
+        return response.json()
+            
+    
+    def fetch_game(self, season, game_type, game_num):
+        game_id = f"{season}0{game_type}0{game_num}"
+        url = self.base_url.format(GAME_ID=game_id)
+        
+        if game_id in self.cache:
+            return self.cache[game_id]
+        
+        if os.path.exists(self.data_path):
+            with open(self.data_path, 'r') as file:
+                data = pd.read_json(file)
+                self.cache[game_id] = data
+                return data
+        
+        print(f"Retrieving data from '{url}'...")
+        data = self._fetch_data_from_api(url)
+        
+        self.cache[game_id] = pd.DataFrame(data)
+        return self.cache[game_id]
+    
+    def fetch_season(self, season, regular_games=NB_MAX_REGULAR_GAMES_PER_SEASON, playoff_games=NB_MAX_PLAYOFF_GAMES_PER_SEASON):
 
-    print(f"Retrieving data from '{url}'...")
-
-    # Use Pandas' built in HTML parser to retrieve the tabular data from the web data
-    # Uses BeautifulSoup4 in the background to do the heavylifting
-    df = pd.read_html(url, header=1)[0]
-
-    # get players which changed teams during a season
-    players_multiple_teams = df[df['Tm'].isin(['TOT'])]
-
-    # filter out players who played on multiple teams
-    df = df[~df['Player'].isin(players_multiple_teams['Player'])]
-    df = df[df['Player'] != "Player"]
-
-    # add the aggregate rows
-    df = df.append(players_multiple_teams, ignore_index=True)
-
-    return df
+        # Fetch regular season games
+        for game_num in tqdm(range(1, regular_games + 1)).set_description("Fetching regular season games"):
+            print(f"Fetching game {game_num} of {regular_games}")
+            self.fetch_game(season, 2, game_num)
+        
+        # Fetch playoff games
+        for game_num in tqdm(range(1, playoff_games + 1)).set_description("Fetching playoff games"):
+            print(f"Fetching game {game_num} of {playoff_games}")
+            self.fetch_game(season, 3, game_num)
+            
