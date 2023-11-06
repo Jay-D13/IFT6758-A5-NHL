fb2d6fe74d47e0301b338a9bde9b3a08eab1b73f
Guillaume Charron <guillaume.charron99@hotmail.com>
2023-11-06 15:50:33 -0500

    Add script to run an experiment

--- ift6758/training/train.py
+++ ift6758/training/train.py
@@ -8,6 +8,22 @@ class BasicModel:
         self.clf = clf
         self.exp_name = exp_name
 
+    def train(self, X_train, y_train):
+        self.clf = self.clf.fit(X_train.values.reshape(-1, 1), y_train)
+        return self.clf
+
+    def evaluate(self, X_val, y_val):
+        y_pred = self.clf.predict(X_val.values.reshape(-1, 1))
+        accuracy = accuracy_score(y_val, y_pred)
+        return y_pred, accuracy
+
+    def get_pred_probabilities(self, X_val):
+        return self.clf.predict_proba(X_val.values.reshape(-1, 1))
+
+    def save(self, model_name_path):
+        with open(model_name_path, 'wb') as f:
+            pickle.dump(self.clf, f)
+
     def run(self, X_train, y_train, X_val, y_val, save_to=None, tags=None):
         if save_to is None:
             save_to = f'../train/exp/{self.exp_name}/'
---
+++ scripts/Milestone2/basic_models.py
@@ -0,0 +1,62 @@
+import argparse
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+import pandas as pd
+from comet_ml import Experiment
+import os
+from ift6758.training.train import BasicModel
+
+def main(opts):
+    # Get Data
+    train_val = pd.read_pickle(opts.data_path)
+    X_all = train_val.drop(['is_goal'], axis=1)
+    y_all = train_val['is_goal']
+
+    # Split into train val
+    X_train, X_val, y_train, y_val = train_test_split(X_all, y_all, test_size=0.3, random_state=42)
+
+    # Setup Experiment
+    exp = Experiment(
+        api_key=os.environ.get('COMET_API_KEY'),
+        workspace='ift6758-a5-nhl',
+        project_name='milestone2'
+    )
+    exp.set_name(opts.exp_name)
+
+    model = BasicModel(clf=LogisticRegression())
+
+    # Train model
+    model.train(X_train, y_train)
+
+    # Evaluate model
+    accuracy, y_pred = model.evaluate(X_val, y_val)
+    exp.log_metric('accuracy', accuracy)
+
+    # Save model
+    model_path = os.path.join(opts.exp_path, opts.exp_name, 'model.pkl')
+    model.save(model_path)
+    exp.log_model('Model', model_path)
+
+    exp.end()
+
+
+
+def parse_opts(known=False):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_path', type=str, default= '../ift6758/features/trainVal/', help='Data path that contains train and val data')
+    parser.add_argument('--exp_path', type=str, default= '../train/', help='Experience path of parent folder')
+    parser.add_argument('--exp_name', type=str, default= 'exp', help='Experience name for comet ml')
+    parser.add_argument('--use_features', nargs='+', type=str, default= '[distance]', help='Feature to train LogisticRegression with')
+    
+    return parser.parse_known_args()[0] if known else parser.parse_args()
+
+def run(**kwargs):
+    opts = parse_opts(True)
+    for k, v in kwargs.items():
+        setattr(opts, k, v)
+    main(opts)
+    return opts
+
+if __name__ == '__main__':
+    opts = parse_opts()
+    main(opts)
