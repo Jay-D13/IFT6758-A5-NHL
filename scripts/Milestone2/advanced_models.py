8c6829b0dd205acb32e7363b8aebd6affa8a3c23
j_haygurlhay_j <jj.kaufman1@gmail.com>
2023-11-11 14:00:39 -0500

    with comet experiment
    for comet_ml

--- scripts/Milestone2/advanced_models.py
+++ scripts/Milestone2/advanced_models.py
@@ -2,7 +2,7 @@ import argparse
 from sklearn.model_selection import train_test_split
 import xgboost as xgb
 import pandas as pd
-#from comet_ml import Experiment
+from comet_ml import Experiment
 import os
 from ift6758.training.trainBoost import AdvancedModel
 
@@ -20,16 +20,16 @@ def main(opts):
     X_train, X_val, y_train, y_val = train_test_split(X_all, y_all, test_size=0.3, random_state=42)
 
     # Setup Experiment
-    #exp = Experiment(
-    #     api_key=os.environ.get('COMET_API_KEY'),
-    #     workspace='ift6758-a5-nhl',
-    #     project_name='milestone2'
-    # )
-    #exp.set_name(opts.exp_name)
-    # tags = opts.use_features
-    # tags.append('XGBoost')
-    # tags.append('AdvancedModel')
-    #exp.add_tags(tags)
+    exp = Experiment(
+        api_key=os.environ.get('COMET_API_KEY'),
+        workspace='ift6758-a5-nhl',
+        project_name='milestone2'
+    )
+    exp.set_name(opts.exp_name)
+    tags = opts.use_features
+    tags.append('XGBoost')
+    tags.append('AdvancedModel')
+    exp.add_tags(tags)
 
     # Train model
     model = AdvancedModel(xgb.XGBClassifier())
@@ -37,14 +37,14 @@ def main(opts):
 
     # Evaluate model
     y_pred, accuracy = model.evaluate(X_val, y_val)
-    #exp.log_metric('accuracy', accuracy)
+    exp.log_metric('accuracy', accuracy)
 
     # Save model
     model_path = os.path.join(opts.exp_path, opts.exp_name, 'model.pkl')
     model.save(model_path)
-    #exp.log_model('Model', model_path)
+    exp.log_model('Model', model_path)
 
-    #exp.end()
+    exp.end()
 def parse_opts(known=False):
     parser = argparse.ArgumentParser()
     parser.add_argument('--data_path', type=str, default= './ift6758/features/TrainValSets.pk', help='Data path that contains train and val data')
