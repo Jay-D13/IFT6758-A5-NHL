c1edccff6444b1262c296d455619a87500c97809
Jay-D13 <jaydaninparis@gmail.com>
2023-11-12 02:21:29 -0500

    forgot to remove LSTM attempt

--- ift6758/training/__init__.py
+++ ift6758/training/__init__.py
@@ -1,4 +1,3 @@
 from ift6758.training.train import BasicModel
 from ift6758.training.trainBoost import AdvancedModel
 from ift6758.training.net_model import Net
-from ift6758.training.LSTM import LSTMModel
