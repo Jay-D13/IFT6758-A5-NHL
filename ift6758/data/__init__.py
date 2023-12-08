8497f067a7efb5af4b967fdc0059319d2d323710
Guillaume Charron <guillaume.charron99@hotmail.com>
2023-12-07 19:05:22 -0500

    Chane api endpoint

--- ift6758/data/__init__.py
+++ ift6758/data/__init__.py
@@ -1,6 +1,6 @@
 from enum import Enum
 
-NHL_GAME_URL = 'https://statsapi.web.nhl.com/api/v1/game/{GAME_ID}/feed/live/'
+NHL_GAME_URL = 'https://api-web.nhle.com/v1/gamecenter/{GAME_ID}/play-by-play'
 NB_MAX_REGULAR_GAMES_PER_SEASON = 1353
 WANTED_EVENTS = ['SHOT', 'GOAL']
 PREVIOUS_EVENTS = ['SHOT', 'GOAL', 'MISSED_SHOT', 'BLOCKED_SHOT', 'FACEOFF', 'HIT', 'TAKEAWAY', 'GIVEAWAY']
