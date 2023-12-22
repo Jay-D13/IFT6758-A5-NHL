a1dceffe518fb488c9ee48c25efc39ad1c556033
Jay-D13 <jaydaninparis@gmail.com>
2023-12-22 05:26:54 -0500

    forgot to change hardcoded service url :(

--- package/ift6758/client/serving_client.py
+++ package/ift6758/client/serving_client.py
@@ -11,9 +11,9 @@ logger = logging.getLogger(__name__)
 class ServingClient:
     def __init__(self):
         
-        # ip = os.environ.get('SERVING_IP', 'ift6758-a5-nhl-serving-1')
-        # port = os.environ.get('SERVING_PORT', '5002')
-        self.base_url = f"http://ift6758-a5-nhl-serving-1:5002"
+        ip = os.environ.get('SERVING_IP')
+        port = os.environ.get('SERVING_PORT', '5002')
+        self.base_url = f"http://{ip}:{port}"
 
         print(f"Initializing client; base URL: {self.base_url}")
         
